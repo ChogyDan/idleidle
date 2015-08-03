cceb8e666c4b72c95ce2c1be00bfc186355287c7
Daniel Hollocher <danielhollocher@gmail.com>
2015-08-03 00:31:31 -0400

    initial commit

---
+++ core.js
@@ -0,0 +1,27 @@
+var myIdle = angular.module('myIdleGame', ["ngStorage"]);
+
+myIdle.controller('coreCtrl', ['$interval', '$scope', '$localStorage', function ($interval, $scope, $localStorage) {
+	var self = this;
+	if($localStorage.idles === undefined) {
+		$localStorage.idles = 0;
+		console.log("idles not defined");
+	}
+	self.idles = $localStorage.idles;
+	self.addIdles = function(toAdd){
+		self.idles += toAdd;
+	}
+
+	self.loop = $interval(function(){
+		self.idles += 1;
+		$localStorage.idles = self.idles;
+
+	}, 1000, 0);
+
+	$scope.$on("$destroy", function(){
+		if(angular.isDefined(self.loop)){
+			$interval.cancel(self.loop);
+			self.loop = undefined;
+		}
+
+	})
+}]);
